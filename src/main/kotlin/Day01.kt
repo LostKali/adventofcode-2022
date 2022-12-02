42ee21c6ec48faa739658187889af53fd10136c4
Kate Grebtsova <taburetka0@gmail.com>
2022-12-02 11:23:21 +0800

    Advent of Code Day 1 puzzles

---
+++ src/main/kotlin/Day01.kt
@@ -0,0 +1,46 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+//    getBiggestSum()
+    getTo3BiggestSum()
+}
+
+private fun getBiggestSum() {
+    val file = Dummy::class.java.getResource("/day01/input.txt").file
+    var a = 0
+    var b = 0
+    File(file).readLines().forEach {
+        if (it.isBlank()) {
+            if (a < b) {
+                a = b
+            }
+            b = 0
+        } else {
+            b += it.toInt()
+        }
+    }
+    println(maxOf(a, b))
+}
+
+private fun getTo3BiggestSum() {
+    val file = Dummy::class.java.getResource("/day01/input.txt").file
+    var sum = 0
+    val topList = File(file).readLines().foldIndexed(ArrayList<Int>()) { idx, topList, candidate ->
+        if (candidate.isNotBlank()) {
+            sum += candidate.toInt()
+        }
+        if (candidate.isBlank() || idx == File(file).readLines().size - 1) {
+            topList.add(sum)
+            topList.sortDescending()
+            if (topList.size > 3) {
+                topList.removeAt(3)
+            }
+            sum = 0
+        }
+        topList
+    }
+
+    println(topList.sum())
+}
+
+class Dummy
